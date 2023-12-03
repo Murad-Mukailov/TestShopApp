c1672682b1a182e082a47e8c14e5adc6f4a8de88
Murad <muradkartal91@gmail.com>
2023-12-03 14:48:33 +0300

    init commit

---
+++ app/src/main/java/com/example/testshopapp/signIn/presentation/SignInViewModel.kt
@@ -0,0 +1,14 @@
+package com.example.testshopapp.signIn.presentation
+
+import android.app.Application
+import android.content.Context
+import android.content.SharedPreferences
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.navigation.fragment.NavHostFragment.Companion.findNavController
+import com.example.testshopapp.R
+
+class SignInViewModel : ViewModel() {
+
+}
